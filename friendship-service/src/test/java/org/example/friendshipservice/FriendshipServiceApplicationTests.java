a52e8fe42c1e09750446bad2fa88ab75b9bab60f
glioualyassine <glioualyassine@gmail.com>
2024-09-29 23:12:57 +0100

    starting last serviceé

---
+++ friendship-service/src/test/java/org/example/friendshipservice/FriendshipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.friendshipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
